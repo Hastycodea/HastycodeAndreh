91ad6448e6aff8025afc4ba105d6121a683540d2
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-10-24 10:11:38 +0300

    pendingSliders

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,45 +1,98 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./Blogs.css";
 import blog from "../../assets/blog.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 const Blogs = () => {
+  
+
+  const slider = useRef();
+  let tx = 0;
+
+  const slideForward = ()=> {
+    if(tx > -50){
+      tx -= -25;
+    }
+    slider.current.style.transform = `translateX(${tx}%)`;
+  }
+  const slideBackward = ()=> {
+    if(tx < 0){
+      tx += 25;
+    }
+    slider.current.style.transform = `translateX(${tx}%)`;
+  }
+
+
   return (
     <div className="blogs">
       <p>
         I love documenting my work and teaching other developers as this is the
         way to learn, below are some of my articles on Medium
       </p>
-      <div className="display-blogs">
-      <div className="blog">
-        <img src={blog} alt="" />
-        <p className="blog-title">pagination in phoenix live view</p>
-        <p className="blog-description">
-          In this article, I will show you how to create migrations in Rails.
-          Migrations are a very important concept in Rails and are used to
-          create, modify, and delete tables in your database.
-        </p>
-        <button>READ MORE</button>
-      </div>
-      <div className="blog">
-        <img src={blog} alt="" />
-        <p className="blog-title">pagination in phoenix live view</p>
-        <p className="blog-description">
-          In this article, I will show you how to create migrations in Rails.
-          Migrations are a very important concept in Rails and are used to
-          create, modify, and delete tables in your database.
-        </p>
-        <button>READ MORE</button>
-      </div>
-      <div className="blog">
-        <img src={blog} alt="" />
-        <p className="blog-title">pagination in phoenix live view</p>
-        <p className="blog-description">
-          In this article, I will show you how to create migrations in Rails.
-          Migrations are a very important concept in Rails and are used to
-          create, modify, and delete tables in your database.
-        </p>
-        <button>READ MORE</button>
-      </div>
+      <FontAwesomeIcon icon={faArrowLeft} className="left-arrow" onClick={slideBackward} />
+      <FontAwesomeIcon icon={faArrowRight} className="right-arrow" onClick={slideForward}/>
+      <div className="display-blogs" ref={slider}>
+        <div className="blog">
+          <img src={blog} alt="" />
+          <p className="blog-title">pagination in phoenix live view</p>
+          <p className="blog-description">
+            In this article, I will show you how to create migrations in Rails.
+            Migrations are a very important concept in Rails and are used to
+            create, modify, and delete tables in your database.
+          </p>
+          <button>READ MORE</button>
+        </div>
+        <div className="blog">
+          <img src={blog} alt="" />
+          <p className="blog-title">pagination in phoenix live view</p>
+          <p className="blog-description">
+            In this article, I will show you how to create migrations in Rails.
+            Migrations are a very important concept in Rails and are used to
+            create, modify, and delete tables in your database.
+          </p>
+          <button>READ MORE</button>
+        </div>
+        <div className="blog">
+          <img src={blog} alt="" />
+          <p className="blog-title">pagination in phoenix live view</p>
+          <p className="blog-description">
+            In this article, I will show you how to create migrations in Rails.
+            Migrations are a very important concept in Rails and are used to
+            create, modify, and delete tables in your database.
+          </p>
+          <button>READ MORE</button>
+        </div>
+        <div className="blog">
+          <img src={blog} alt="" />
+          <p className="blog-title">pagination in phoenix live view</p>
+          <p className="blog-description">
+            In this article, I will show you how to create migrations in Rails.
+            Migrations are a very important concept in Rails and are used to
+            create, modify, and delete tables in your database.
+          </p>
+          <button>READ MORE</button>
+        </div>
+        <div className="blog">
+          <img src={blog} alt="" />
+          <p className="blog-title">pagination in phoenix live view</p>
+          <p className="blog-description">
+            In this article, I will show you how to create migrations in Rails.
+            Migrations are a very important concept in Rails and are used to
+            create, modify, and delete tables in your database.
+          </p>
+          <button>READ MORE</button>
+        </div>
+        <div className="blog">
+          <img src={blog} alt="" />
+          <p className="blog-title">pagination in phoenix live view</p>
+          <p className="blog-description">
+            In this article, I will show you how to create migrations in Rails.
+            Migrations are a very important concept in Rails and are used to
+            create, modify, and delete tables in your database.
+          </p>
+          <button>READ MORE</button>
+        </div>
       </div>
     </div>
   );
