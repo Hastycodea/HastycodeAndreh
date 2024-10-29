1d3226888bb3efb3ab2c96366b41793835096e4d
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-10-29 09:50:24 +0300

    slickslide

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -25,14 +25,39 @@ const Blogs = () => {
   // }
   const settings = {
     dots: true,
+    className: "center",
     speed: 500,
     slidesToShow: 3,
-    slidesToScroll: 1,
+    slidesToScroll: 3,
     infinite: true,
     autoplay: true,
-    autoplaySpeed: 1000,
-    centerMode: true,
-    centerPadding: "20px",
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      } 
+    ],
   };
 
   return (
